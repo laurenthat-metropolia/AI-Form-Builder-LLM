796514d149a3d4ab1da37c6ef9a6325fce73438f
ericaskari <ericaskari@gmail.com>
2023-11-04 02:21:36 +0200

    Train CICD

--- train.py
+++ train.py
@@ -6,4 +6,4 @@ from ultralytics import YOLO
 if __name__ == "__main__":
     print(torch.cuda.is_available())
     model = YOLO("yolov8n.pt")
-    model.train(data=os.path.join(os.getcwd(), 'dataset', "data.yaml"),epochs=5,model="ai-form-builder.pt", imgsz=224)
+    model.train(data=os.path.join(os.getcwd(), 'dataset', "data.yaml"),epochs=2,model="yolov8n.pt", imgsz=224)
