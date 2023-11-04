e3521457098a19fc36ad75a86f862a6e2d9155ac
Mohammad Askari (Eric) <32340808+ericaskari@users.noreply.github.com>
2023-11-04 20:56:11 +0200

    Train cicd (#3)
    Train CICD

--- train.py
+++ train.py
@@ -2,8 +2,24 @@ import os
 
 import torch
 from ultralytics import YOLO
+from roboflow import Roboflow
+from dotenv import load_dotenv
+
+load_dotenv()  # take environment variables from .env.
+
+apikey = os.getenv("APP_ROBEFLOW_API_KEY")  # ****
+version = os.getenv("APP_ROBEFLOW_DATASET_VERSION")  # 2
+epochs = os.getenv("APP_EPOCHS")  # 2
+baseModel = os.getenv("APP_IMAGE_BASE")  # yolov8n.pt
+imgsz = os.getenv("APP_IMAGE_SIZE")  # 224
+
+rf = Roboflow(api_key=apikey)
+
+project = rf.workspace().project(project_name="draw2form")
+
+dataset = project.version(version).download(model_format="yolov8", location="dataset", overwrite=True)
 
 if __name__ == "__main__":
     print(torch.cuda.is_available())
-    model = YOLO("yolov8n.pt")
-    model.train(data=os.path.join(os.getcwd(), 'dataset', "data.yaml"),epochs=5,model="ai-form-builder.pt", imgsz=224)
+    model = YOLO(baseModel)
+    model.train(data=os.path.join(os.getcwd(), 'dataset', "data.yaml"), epochs=epochs, model=baseModel, imgsz=imgsz)
