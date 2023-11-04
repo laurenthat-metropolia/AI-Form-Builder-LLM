39af68e7707f5cb976503b65adff30071eb1f92f
ericaskari <ericaskari@gmail.com>
2023-11-04 21:31:21 +0200

    Train CICD

--- train.py
+++ train.py
@@ -7,11 +7,12 @@ from dotenv import load_dotenv
 
 load_dotenv()  # take environment variables from .env.
 
-apikey = os.getenv("APP_ROBEFLOW_API_KEY")  # ****
-version = os.getenv("APP_ROBEFLOW_DATASET_VERSION")  # 2
+apikey = os.getenv("APP_ROBOFLOW_API_KEY")  # ****
+version = os.getenv("APP_ROBOFLOW_DATASET_VERSION")  # 2
 epochs = os.getenv("APP_EPOCHS")  # 2
-baseModel = os.getenv("APP_IMAGE_BASE")  # yolov8n.pt
+base_model = os.getenv("APP_IMAGE_BASE")  # yolov8n.pt
 imgsz = os.getenv("APP_IMAGE_SIZE")  # 224
+name = os.getenv("APP_MODEL_NAME")  # 224
 
 rf = Roboflow(api_key=apikey)
 
@@ -19,7 +20,9 @@ project = rf.workspace().project(project_name="draw2form")
 
 dataset = project.version(int(version)).download(model_format="yolov8", location="dataset", overwrite=True)
 
+data = os.path.join(os.getcwd(), 'dataset', "data.yaml")
+
 if __name__ == "__main__":
     print(torch.cuda.is_available())
-    model = YOLO(baseModel)
-    model.train(data=os.path.join(os.getcwd(), 'dataset', "data.yaml"), epochs=int(epochs), model=baseModel, imgsz=int(imgsz))
+    model = YOLO(base_model)
+    model.train(data=data, epochs=int(epochs), model=base_model, imgsz=int(imgsz), name=name)
