ccf0f0d9e47eb486e8f67bfdae8fe82699916397
Mohammad Askari (Eric) <32340808+ericaskari@users.noreply.github.com>
2023-11-09 18:29:25 +0200

    Features/chatgpt (#23)
    * Previews and Cleanups
    
    * Previews and Cleanups
    
    * Previews and Cleanups
    
    * [skip ci] Previews and Cleanups
    
    * [skip ci] Previews and Cleanups
    
    * Previews and Cleanups
    
    * Previews and Cleanups
    
    * Previews and Cleanups

---
+++ chatgpt-api/app/configurations/environment.ts
@@ -0,0 +1,8 @@
+import { cleanEnv, host, str } from 'envalid';
+import { config } from 'dotenv';
+
+// Load environment variables from a file named .env
+config();
+export const environment = cleanEnv(process.env, {
+    APP_OPENAI_API_KEY: str(),
+});
