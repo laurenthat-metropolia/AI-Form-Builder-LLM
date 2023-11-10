18f03679c8f29079042587be6b505f02923e1665
Mohammad Askari (Eric) <32340808+ericaskari@users.noreply.github.com>
2023-11-10 09:13:54 +0200

    New Model added (#24)
    * New Model added
    
    * ChatGPT added

--- chatgpt-api/app/main.ts
+++ chatgpt-api/app/main.ts
@@ -4,6 +4,9 @@ import { configParsers } from './configurations/configParsers.js';
 
 import { ErrorRequestHandler } from 'express-serve-static-core';
 import morgan from 'morgan';
+import { configOpenAi } from './configurations/configOpenAi.js';
+import { OpenAI } from 'openai';
+
 // Express App
 const app = express();
 
@@ -18,20 +21,29 @@ app.use(
 // Parsing capabilities for body of request.
 configParsers(app);
 
-const router = express.Router()
+const openai = configOpenAi();
+
+const router = express.Router();
 
 app.use('/chatgpt', router);
 
-router.get('/', async (req: Request, res: Response): Promise<void> => {
-    res.status(404).send({
-        message: "Did you mean to send a POST request instead of GET?"
+router.get('/', async (req: Request, res: Response) => {
+    const body: OpenAI.ChatCompletionCreateParams = {
+        messages: [{ role: 'user', content: 'Say this is a test' }],
+        model: 'gpt-3.5-turbo',
+    };
+    const chatCompletion = await openai.chat.completions.create(body);
+
+    res.send({
+        input: body,
+        output: chatCompletion,
     });
     return;
 });
 
 router.post('/', async (req: Request, res: Response): Promise<void> => {
     res.status(501).send({
-        message: "CHATGPT NOT IMPLEMENTED YET."
+        message: 'CHATGPT NOT IMPLEMENTED YET.',
     });
     return;
 });
@@ -54,11 +66,11 @@ router.use((async (err, req: Request, res: Response, next): Promise<void> => {
 app.listen(8002, '0.0.0.0', (): void => {
     console.log('Started listening on port 8002');
 });
-process.on('SIGTERM', function() {
+process.on('SIGTERM', function () {
     console.log('\ncaught SIGTERM, stopping gracefully');
     process.exit(1);
 });
-process.on('SIGINT', function() {
+process.on('SIGINT', function () {
     console.log('\ncaught SIGINT, stopping gracefully');
     process.exit();
 });
