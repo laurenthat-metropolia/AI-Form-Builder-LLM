8b4b11c4cec5fbbb726884266cff71c21fd40561
ericaskari <ericaskari@gmail.com>
2023-11-17 20:42:42 +0200

    nx

--- draw2form/apps/web/src/main.tsx
+++ draw2form/apps/web/src/main.tsx
@@ -1,16 +1,38 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-
+import { createBrowserRouter, RouterProvider, Router } from 'react-router-dom';
 import App from './app/app';
+import { Upload } from './app/pages/Upload';
+import { UploadDetails } from './app/pages/UploadDetails';
+import { AndroidLinkLoginMissingApp } from './app/android/auth/login/AndroidLinkLoginMissingApp';
+import NxWelcome from './app/nx-welcome';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+        children: [
+            {
+                path: '/upload',
+                element: <Upload />,
+            },
+            {
+                path: '/upload/:id',
+                element: <UploadDetails />,
+            },
+        ],
+    },
+    {
+        path: '/android/auth/login',
+        element: <AndroidLinkLoginMissingApp />,
+    },
+]);
+
 root.render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>
+    <StrictMode>
+        <NxWelcome title=""></NxWelcome>
+        {/*<RouterProvider router={router} />*/}
+    </StrictMode>,
 );
