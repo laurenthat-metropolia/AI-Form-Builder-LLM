fd07c489dd7181f3874243b4a2d04c361261275a
ericaskari <ericaskari@gmail.com>
2023-11-21 19:44:22 +0200

    Backend Imple

--- apps/api-new/jest.config.ts
+++ apps/api-new/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-    displayName: 'api-new',
+    displayName: 'api',
     preset: '../../jest.preset.js',
     testEnvironment: 'node',
     transform: {
         '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
     },
     moduleFileExtensions: ['ts', 'js', 'html'],
-    coverageDirectory: '../../coverage/apps/api-new',
+    coverageDirectory: '../../coverage/apps/api',
 };
