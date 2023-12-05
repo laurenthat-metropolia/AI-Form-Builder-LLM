0d5820d6c0eebcd0f16e95c66f8ea82c8911e8a6
Mohammad Askari (Eric) <32340808+ericaskari@users.noreply.github.com>
2023-12-05 16:09:17 +0200

    Ordering (#61)

--- apps/api/src/app/services/form.service.ts
+++ apps/api/src/app/services/form.service.ts
@@ -1,7 +1,7 @@
 import { prisma } from '../databases/userDatabase';
 import { Form } from '@prisma/client';
 import { randomUUID } from 'crypto';
-import { SupportedFormComponent } from '@draw2form/shared';
+import { Resources, SupportedFormComponent, ValueOf } from '@draw2form/shared';
 
 export const populateUserFormBasedOnChatGPTResponse = async (
     name: string,
@@ -129,6 +129,96 @@ const countFormFields = async (formId: string): Promise<number> => {
     return checkboxes + textFields + toggleSwitches + buttons + labels + images;
 };
 
+const updateResourceOrder = async (resource: ValueOf<typeof Resources>, id: string, order: number) => {
+    const args = {
+        where: {
+            id: id,
+        },
+        data: {
+            order,
+        },
+    };
+    switch (resource) {
+        case Resources.FormLabel:
+            await prisma.formLabel.update(args);
+            break;
+        case Resources.FormImage:
+            await prisma.formImage.update(args);
+            break;
+        case Resources.FormToggleSwitch:
+            await prisma.formToggleSwitch.update(args);
+            break;
+        case Resources.FormButton:
+            await prisma.formButton.update(args);
+            break;
+        case Resources.FormCheckbox:
+            await prisma.formCheckbox.update(args);
+            break;
+        case Resources.FormTextField:
+            await prisma.formTextField.update(args);
+            break;
+    }
+};
+const validateAndFixFormFieldOrders = async (formId: string) => {
+    const formFields = await getFormFields(formId);
+
+    const everythingGood = formFields.every((item, index) => item.order === index);
+    if (everythingGood) {
+        console.log(`[OrderValidation] Form ${formId} Healthy`);
+        return true;
+    } else {
+        console.log(`[OrderValidation] Form ${formId} Order mismatch. fixing....`);
+        for (const formField of formFields) {
+            const index = formFields.indexOf(formField);
+            await updateResourceOrder(formField.resource, formField.id, index);
+        }
+    }
+};
+
+const getFormFields = async (formId: string) => {
+    const checkboxes = await prisma.formCheckbox.findMany({
+        where: {
+            formId: formId,
+        },
+    });
+    const textFields = await prisma.formTextField.findMany({
+        where: {
+            formId: formId,
+        },
+    });
+    const toggleSwitches = await prisma.formToggleSwitch.findMany({
+        where: {
+            formId: formId,
+        },
+    });
+    const buttons = await prisma.formButton.findMany({
+        where: {
+            formId: formId,
+        },
+    });
+    const labels = await prisma.formLabel.findMany({
+        where: {
+            formId: formId,
+        },
+    });
+    const images = await prisma.formImage.findMany({
+        where: {
+            formId: formId,
+        },
+    });
+
+    return [
+        checkboxes.map((x) => ({ id: x.id, order: x.order, resource: Resources.FormCheckbox, item: x })),
+        textFields.map((x) => ({ id: x.id, order: x.order, resource: Resources.FormTextField, item: x })),
+        toggleSwitches.map((x) => ({ id: x.id, order: x.order, resource: Resources.FormToggleSwitch, item: x })),
+        buttons.map((x) => ({ id: x.id, order: x.order, resource: Resources.FormButton, item: x })),
+        labels.map((x) => ({ id: x.id, order: x.order, resource: Resources.FormLabel, item: x })),
+        images.map((x) => ({ id: x.id, order: x.order, resource: Resources.FormImage, item: x })),
+    ]
+        .flat()
+        .sort((a, b) => a.order - b.order);
+};
+
 const findOnePopulatedById = async (id: string) => {
     const item = await prisma.form.findFirst({
         where: {
@@ -164,4 +254,7 @@ export const forms = {
     findPopulatedManyByOwnerId: findPopulatedManyByOwnerId,
     findOnePopulatedById: findOnePopulatedById,
     countFormFields: countFormFields,
+    validateAndFixFormFieldOrders: validateAndFixFormFieldOrders,
+    getFormFields: getFormFields,
+    updateResourceOrder: updateResourceOrder,
 };
